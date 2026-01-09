ebe150c39defc8929f4f4a6d02c34238baa2b7dc
aKqir24 <Akqir@protonmail.com>
2026-01-09 23:59:23 +0800

    add: gui code files

---
+++ gui/main.py
@@ -0,0 +1,23 @@
+import sys
+
+config: dict = {
+    # Settings
+    "RESET": false
+    "RELOAD": false
+    "VERBOSE": false
+
+    # Setup
+    "THEME_MODE":"dark"
+    "INSTALL_GTK_THEME": True
+    "INSTALL_ICON_THEME": True
+    "ANIMATED_WALLPAPER": False
+    "THEME_ACCENT_COLOR": 2
+    "WALLPAPER_CYCLE": "static"
+
+    # Pywal
+    "PYWAL_BACKEND": "wal"
+    "THEME_ACCENT_COLOR": 2
+    "PYWAL_LIGHT_THEME": True
+    "WALLPAPER_BACKEND": "none"
+    "PYWAL_COLORSCHEME": "light"
+}  
